7bb682fe587531b015ca54ccd0487880ac463ea8
kibarke <kibarkework@gmail.com>
2024-01-15 01:20:49 -0500

    homework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 const startBtn = document.querySelector ('.start-btn');
 const popupInfo = document.querySelector ('.popup-info');
 const exitBtn = document.querySelector ('.exit-btn');
+const main = document.querySelector ('.main');
 
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
+    main.classList.add('active');
 }
 
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
+
 
