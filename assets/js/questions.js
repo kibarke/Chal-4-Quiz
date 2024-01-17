abe067340812838805e5e5caea6dd1b8d42da6cc
kibarke <kibarkework@gmail.com>
2024-01-16 20:51:03 -0500

    Finally added the questions for the quiz, having trouble on next-btn

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        questions: "Who plays Iron-Man?",
+        answer: "C. Robert Downey Junior",
+        options: [
+            "A. Tom Hiddlestone",
+            "B. Chris Hemsworth",
+            "C. Robert Downey Junior",
+            "D. Chris Evans"
+        ]
+    },
+    {
+        numb: 2,
+        questions: "What is the Capital of the United States?",
+        answer: "B. Washington D.C.",
+        options: [
+            "A. Los Angeles",
+            "B. Washington D.C.",
+            "C. Philadelphia",
+            "D. New York City"
+        ]
+    },
+    {
+        numb: 3,
+        questions: "What is Walt Disney’s main character?",
+        answer: "A. Mickey Mouse",
+        options: [
+            "A. Mickey Mouse",
+            "B. Minnie Mouse",
+            "C. Mikey Mouse",
+            "D. Steamboat Willie"
+        ]
+    },
+    {
+        numb: 4,
+        questions: "How many horcruxes does Voldemort have?",
+        answer: "C. 7",
+        options: [
+            "A. 9",
+            "B. 8",
+            "C. 7",
+            "D. 6"
+        ]
+    },
+    {
+        numb: 5,
+        questions: "In Greek Mythology, Kronos was killed by ____?",
+        answer: "D. Zeus",
+        options: [
+            "A. Percy Jackson",
+            "B. Kratos",
+            "C. Rhea",
+            "D. Zeus"
+        ]
+    }
+
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ const popupInfo = document.querySelector ('.popup-info');
 const exitBtn = document.querySelector ('.exit-btn');
 const main = document.querySelector ('.main');
 const continueBtn = document.querySelector ('.continue-btn');
-const qiuzSection = document.querySelector ('.quiz-section');
-
+const quizSection = document.querySelector ('.quiz-section');
+const quizBox = document.querySelector ('.quiz-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -17,7 +17,25 @@ exitBtn.onclick = () => {
 }
 
 continueBtn.onclick = () => {
-    qiuzSection.classList.add('active');
+    quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
 }
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    questionCount++;
+    showQuestions(questionCount);
+}
+
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-test');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+  }
